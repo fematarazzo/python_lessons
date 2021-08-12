90a72e7ee5084f82f0fbf8b49254922a1fe4ed76
Felipe <felipemps@protonmail.com>
2021-08-12 09:35:07 -0300

    lesson 2 - strip function

---
+++ strip.py
@@ -0,0 +1,13 @@
+left_whitespace = '   Left whitespace'
+right_whitespace = 'Right whitespace   '
+both_whitespace = '   Both sides whitespace   '
+
+# Strip functions
+
+example_one = left_whitespace.lstrip()
+example_two = right_whitespace.rstrip()
+example_three = both_whitespace.strip()
+
+print(example_one)
+print(example_two)
+print(example_three)
