11941827feb57210e94f1832057402ea6d795bb1
Felipe <felipemps@protonmail.com>
2021-08-13 16:11:29 -0300

    text manipulation lesson added

---
+++ text_manipulation.py
@@ -0,0 +1,11 @@
+# Text manipulation functions
+
+message = "Hello world!"
+
+message_upcase = message.upper()
+message_lowercase = message.lower()
+message_titlecase = message.title()
+
+print(message_upcase)
+print(message_lowercase)
+print(message_titlecase)
