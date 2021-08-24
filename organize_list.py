a364ed5f90ccd84784b6b75cc3314ab7711e64fc
Felipe Matarazzo <fematarazzo>
2021-08-24 11:23:50 -0300

    How to organize a list lesson

---
+++ organize_list.py
@@ -0,0 +1,29 @@
+# Organizing a list
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# Sorting a list permanently with sort() method
+cars.sort()
+print(cars)
+
+# Sorting a list temporarily with the sorted() function
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+# Printing a list in reverse order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+# Finding the length of a list
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+len(cars)
